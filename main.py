407eb60a6e7396567593a24ef109344704515715
Michal <malek.michal.k@gmail.com>
2022-01-15 19:19:23 +0100

    Snake moving: part 1

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-import turtle
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 start_pos = [(0, 0), (-20, 0), (-40, 0)]
+snake_segments = []
 
 for pos in start_pos:
     segment = Turtle(shape="square")
+    segment.penup()
     segment.color("white")
     segment.goto(pos)
+    snake_segments.append(segment)
+    screen.update()
+
+is_game_on = True
+while is_game_on:
+    for snake_seg in snake_segments:
+        snake_seg.forward(10)
+        screen.update()
+        time.sleep(1)
 
 screen.exitonclick()

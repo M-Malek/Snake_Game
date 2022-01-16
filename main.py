26dbd55404ccea6ed9939ded2960908378012818
Michal <malek.michal.k@gmail.com>
2022-01-16 12:17:30 +0100

    Add methods to controling snake direction

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from turtle import Screen, Turtle
 from snake import Snake
 import time
 
-
+# Screen initialize + screen setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+# Create snake from Snake class
 snake = Snake()
 
+# Listen to user controls: how to control snake
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# Main game loop
 is_game_on = True
 while is_game_on:
     screen.update()
--- snake.py
+++ snake.py
@@ -1,22 +1,49 @@
+from turtle import Turtle
+
+
 class Snake:
+    # Initialize snake class
     def __init__(self):
-        self.start_pos = [(0, 0), (-20, 0), (-40, 0)]
+        self.START_POS = [(0, 0), (-20, 0), (-40, 0)]
+        self.MOVE_VALUE = 20
         self.snake_segments = []
         self.initialize_snake()
 
+    # Set snake on board
     def initialize_snake(self):
-        from turtle import Turtle
-        for pos in self.start_pos:
+        for pos in self.START_POS:
             segment = Turtle(shape="square")
             segment.penup()
             segment.color("white")
             segment.goto(pos)
             self.snake_segments.append(segment)
 
+    # Let snake move forward (note: this method is responsible only for moving snake by MOVE_VALUE, not for snake
+    # direction)
     def move(self):
-        from turtle import Turtle
         for seg_int in range(len(self.snake_segments) - 1, 0, -1):
             x_pos = self.snake_segments[seg_int - 1].xcor()
             y_pos = self.snake_segments[seg_int - 1].ycor()
             self.snake_segments[seg_int].goto(x_pos, y_pos)
-        self.snake_segments[0].forward(20)
+        self.snake_segments[0].forward(self.MOVE_VALUE)
+
+    # Method responsible for prevent instant change of snake direction
+    def prevent_instant_change(self):
+        pass
+
+    # Methods responsible for changing snake direction
+    def up(self):
+        head = self.snake_segments[0]
+        head.setheading(90)
+
+    def down(self):
+        head = self.snake_segments[0]
+        head.setheading(270)
+
+    def left(self):
+        head = self.snake_segments[0]
+        head.setheading(180)
+
+    def right(self):
+        head = self.snake_segments[0]
+        head.setheading(0)

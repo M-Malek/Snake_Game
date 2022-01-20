94824fd9a4f0ad15c344f6aba01fa97a939911e2
Michal <malek.michal.k@gmail.com>
2022-01-20 21:58:17 +0100

    Project reorganization: work summary, created TODO list with 4 main tasks

--- gui/gui.py
+++ gui/gui.py
@@ -14,6 +14,9 @@ class GUI:
         self.gui_controls = Button()
         self.gui_author = Button()
         self.gui_quit = Button()
+        self.close_button = Button()
+        # self.controls = GuiControls() # error: infinite loop -> child class is calling main class which is calling
+        # child class
         canvas = tkinter.Canvas(master=self.window, width=self.screen_width, height=self.screen_height)
         background = tkinter.PhotoImage(file="gui/snake_title_screen.png")
         background_label = Label(image=background)
@@ -27,15 +30,14 @@ class GUI:
     def hide_menu(self):
         self.start_button.destroy()
         self.gui_controls.destroy()
-        #self.gui_author.destroy()
+        self.gui_author.destroy()
         self.gui_quit.destroy()
 
     def menu_reload(self):
+        self.close_button.destroy()
+
         self.start_button = Button(self.window, text="           Start           ", command=self.start_game)
         self.start_button.grid(row=2, column=1)
-
-        self.gui_controls = Button(self.window, text="           Controls          ", command=self.hide_menu)
-        self.gui_controls.grid(row=3, column=1)
 
         self.gui_author = Button(self.window, text="           Author           ", command=self.menu_reload)
         self.gui_author.grid(row=4, column=1)
@@ -48,23 +50,48 @@ class GUI:
         main_game()
         GUI()
 
+    def gui_controls_sidebar(self):
+        # Window with controls attach to game window
+        pass
+
+
+class GuiControls(GUI):
+    # Class has to take from parent only master window
+    def __init__(self):
+        super().__init__()
+        self.keys_label = Label()
+        self.close_button = Button()
+        self.keys = tkinter.PhotoImage(file="gui/snake_keys.png")
+
+        self.gui_controls = Button(self.window, text="           Controls          ",
+                                   command=self.gui_show_controls)
+        self.gui_controls.grid(row=3, column=1)
+
     def gui_show_controls(self):
         # Destroy all buttons, show controls. Exit button will reload menu
         # Arrow keys picture:
         # https://image.shutterstock.com/image-vector/arrow-button-on-keyboard-icon-260nw-339331691.jpg
         self.hide_menu()
+        self.keys_label = Label(self.window, image=self.keys)
+        self.keys_label.grid(row=5, column=1)
+        self.close_button = Button(self.window, text="Close", command=self.gui_show_controls_destroy)
+        self.close_button.grid(row=8, column=1)
 
-        quit_button = Button(text="Close", command=self.menu_reload)
-        pass
+    def gui_show_controls_destroy(self):
+        self.keys_label.destroy()
+        self.close_button.destroy()
 
-    def gui_controls_sidebar(self):
-        # Window with controls attach to game window
-        pass
+
+class GuiAuthor(GUI):
+    def __init__(self):
+        super().__init__()
 
     def author(self):
         # Destroy all buttons, show author. Exit button will reload menu
         Label(self.window, text="Created by Michal Malek. Upgraded version of Snake Game from 100 Days of Code Python "
                                 "Challenge")
 
-
-# screen = GUI()
+# TODO 1: Przepisać klasę GUI: musi zawierać jedynie podstawowe okno menu z tłem
+# TODO 2: Każdy przycisk zapisać jako osobną klasę
+# TODO 3: Okienko z pomocą przyczepione do okna głównego
+# TODO 4: Okno gry pojawiające się w oknie głównym a nie nowym oknie

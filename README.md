bab2564d405856f0ec7ea77ea2e36a344a66ea91
Michal <malek.michal.k@gmail.com>
2022-01-21 22:50:55 +0100

    Version 1.1 - completed GUI with all functionality

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Snake Game v1.0
+Snake Game v1.1
 
 Classic snake game created during 100 days of Code Python Challenge:
 https://www.udemy.com/share/103IHM3@VBQYo05_-WD9j0wSKiukqeS490POl0L7ZnHd_Ya6gOSvyxuoNjVmaZIcEGQjMtmc/
@@ -6,6 +6,17 @@ https://www.udemy.com/share/103IHM3@VBQYo05_-WD9j0wSKiukqeS490POl0L7ZnHd_Ya6gOSv
 Controls:
 Use keyboard arrows to control snake
 
+Keys control picture from Shutterstock:
+https://image.shutterstock.com/image-vector/arrow-button-on-keyboard-icon-260nw-339331691.jpg
+
+Game rules:
+- snake cannot hit the wall or your tail
+- score by collect red points on map
+
 Game end:
 - snake hit your tail
 - snake hit on wall
+
+Used technology:
+- Python version 3.8
+- tkInter
--- gui/gui.py
+++ gui/gui.py
@@ -15,8 +15,14 @@ class GUI:
         self.gui_author = Button()
         self.gui_quit = Button()
         self.close_button = Button()
+
+        self.keys = tkinter.PhotoImage(file="gui/snake_keys.png")
+        self.keys_label = Label()
+        self.keys_description_label = Label()
         # self.controls = GuiControls() # error: infinite loop -> child class is calling main class which is calling
         # child class
+        self.author_label = Label()
+
         canvas = tkinter.Canvas(master=self.window, width=self.screen_width, height=self.screen_height)
         background = tkinter.PhotoImage(file="gui/snake_title_screen.png")
         background_label = Label(image=background)
@@ -32,14 +38,20 @@ class GUI:
         self.gui_controls.destroy()
         self.gui_author.destroy()
         self.gui_quit.destroy()
+        # for widget in self.window.winfo_children():
+        #     widget.destroy()
 
     def menu_reload(self):
-        self.close_button.destroy()
+        # self.close_button.destroy()
 
         self.start_button = Button(self.window, text="           Start           ", command=self.start_game)
         self.start_button.grid(row=2, column=1)
 
-        self.gui_author = Button(self.window, text="           Author           ", command=self.menu_reload)
+        self.gui_controls = Button(self.window, text="           Controls          ",
+                                   command=self.gui_show_controls)
+        self.gui_controls.grid(row=3, column=1)
+
+        self.gui_author = Button(self.window, text="           Author           ", command=self.gui_show_author)
         self.gui_author.grid(row=4, column=1)
 
         self.gui_quit = Button(self.window, text="           Quit          ", command=self.window.destroy)
@@ -50,48 +62,39 @@ class GUI:
         main_game()
         GUI()
 
-    def gui_controls_sidebar(self):
-        # Window with controls attach to game window
-        pass
-
-
-class GuiControls(GUI):
-    # Class has to take from parent only master window
-    def __init__(self):
-        super().__init__()
-        self.keys_label = Label()
-        self.close_button = Button()
-        self.keys = tkinter.PhotoImage(file="gui/snake_keys.png")
-
-        self.gui_controls = Button(self.window, text="           Controls          ",
-                                   command=self.gui_show_controls)
-        self.gui_controls.grid(row=3, column=1)
-
     def gui_show_controls(self):
         # Destroy all buttons, show controls. Exit button will reload menu
         # Arrow keys picture:
         # https://image.shutterstock.com/image-vector/arrow-button-on-keyboard-icon-260nw-339331691.jpg
         self.hide_menu()
         self.keys_label = Label(self.window, image=self.keys)
-        self.keys_label.grid(row=5, column=1)
+        self.keys_label.grid(row=5, column=2)
+
+        self.keys_description_label = Label(self.window, text="Use keys arrows to control your Snake. Press Up key to "
+                                                              "go up, \n Down key to go down and Left and Right keys "
+                                                              "to turn left and right.", bg='#fff', fg='#f00')
+        self.keys_description_label.grid(row=7, column=2)
         self.close_button = Button(self.window, text="Close", command=self.gui_show_controls_destroy)
         self.close_button.grid(row=8, column=1)
 
     def gui_show_controls_destroy(self):
         self.keys_label.destroy()
         self.close_button.destroy()
+        self.keys_description_label.destroy()
+        self.menu_reload()
 
+    def gui_show_author(self):
+        self.hide_menu()
+        self.author_label = Label(self.window, text="Created by Michal Malek. Upgraded version of Snake Game from 100 "
+                                                    "Days of Code Python "
+                                                    "Challenge. \n Keys picture from Shutterstock: \n "
+                                                    "'https://image.shutterstock.com/image-vector/arrow-button-on-keybo"
+                                                    "ard-icon-260nw-339331691.jpg' ", bg='#fff', fg='#f00')
+        self.author_label.grid(row=6, column=1)
+        self.close_button = Button(self.window, text="Close", command=self.gui_show_author_destroy)
+        self.close_button.grid(row=8, column=1)
 
-class GuiAuthor(GUI):
-    def __init__(self):
-        super().__init__()
-
-    def author(self):
-        # Destroy all buttons, show author. Exit button will reload menu
-        Label(self.window, text="Created by Michal Malek. Upgraded version of Snake Game from 100 Days of Code Python "
-                                "Challenge")
-
-# TODO 1: Przepisać klasę GUI: musi zawierać jedynie podstawowe okno menu z tłem
-# TODO 2: Każdy przycisk zapisać jako osobną klasę
-# TODO 3: Okienko z pomocą przyczepione do okna głównego
-# TODO 4: Okno gry pojawiające się w oknie głównym a nie nowym oknie
+    def gui_show_author_destroy(self):
+        self.close_button.destroy()
+        self.author_label.destroy()
+        self.menu_reload()
